51ea891fc038b6a128ffbf35207adf37e94fdd61
Zacheus <segunzacheusi@gmail.com>
2025-12-28 17:45:43 +0100

    fix(circuits): correct include path and update Power of Tau download URL

--- circuits/setup.sh
+++ circuits/setup.sh
@@ -18,7 +18,7 @@ circom kyc.circom --r1cs --wasm --sym
 # 2. Download Power of Tau (if not exists)
 if [ ! -f "pot12_final.ptau" ]; then
     echo "📥 Downloading Power of Tau file..."
-    curl -L https://hermez.s3-eu-west-1.amazonaws.com/powersOfTau28_hez_final_12.ptau -o pot12_final.ptau
+    curl -L https://storage.googleapis.com/zkevm/ptau/powersOfTau28_hez_final_12.ptau -o pot12_final.ptau
 fi
 
 # 3. Groth16 Setup
